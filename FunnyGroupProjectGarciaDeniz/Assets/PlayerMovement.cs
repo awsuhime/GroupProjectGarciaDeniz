f3cb8686d4406d568766c76348368e894f0cd2cb
awsuhime <awsuhime@gmail.com>
2024-10-08 15:27:31 -0700

    Added assets, player, and basic world

---
+++ FunnyGroupProjectGarciaDeniz/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 1;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
