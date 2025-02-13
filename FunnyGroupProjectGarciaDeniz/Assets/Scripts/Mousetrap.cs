b4a06c0c393a8d025b578164bcf85845dce727d2
awsuhime <awsuhime@gmail.com>
2025-02-13 11:46:01 -0800

    Nodes

--- FunnyGroupProjectGarciaDeniz/Assets/Scripts/Mousetrap.cs
+++ FunnyGroupProjectGarciaDeniz/Assets/Scripts/Mousetrap.cs
@@ -8,21 +8,53 @@ public class Mousetrap : MonoBehaviour
     public float verticalSpeed = 0.2f;
     public float startingY;
     public float multiplier = 1;
+
+    public bool idleBobbing = true;
+    public bool usesNodes = false;
+    public GameObject[] nodes;
+    private int currentNode;
+    public float nodeSpeed = 1.5f;
+    public float nodeRange = 0.2f;
+
     void Start()
     {
         startingY = transform.position.y;
+        foreach (GameObject i in nodes)
+        {
+            i.transform.SetParent(null);
+        }
     }
     void Update()
     {
-        transform.Translate(0, verticalSpeed * Time.deltaTime * multiplier, 0);
+        if (idleBobbing)
+        {
+            transform.Translate(0, verticalSpeed * Time.deltaTime * multiplier, 0);
 
-        if (transform.position.y > startingY + verticalRange)
-        {
-            multiplier = -1;
+            if (transform.position.y > startingY + verticalRange)
+            {
+                multiplier = -1;
+            }
+            else if (transform.position.y < startingY - verticalRange)
+            {
+                multiplier = 1;
+            }
         }
-        else if (transform.position.y < startingY - verticalRange)
+        if (usesNodes)
         {
-            multiplier = 1;
+            Vector3 newpos = new Vector3(transform.position.x, startingY, 0);
+
+            if (Vector3.Distance(newpos, nodes[currentNode].transform.position) < nodeRange)
+            {
+                currentNode++;
+                if (currentNode == nodes.Length)
+                {
+                    currentNode = 0;
+                }
+            }
+            else
+            {
+                transform.Translate((nodes[currentNode].transform.position - newpos).normalized * nodeSpeed * Time.deltaTime);
+            }
         }
     }
 }
